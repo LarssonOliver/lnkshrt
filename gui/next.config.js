c91781efba23be59df1e7469321f03be6b125610
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-02-22 18:33:13 +0100

    gui wip

--- gui/next.config.js
+++ gui/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.API_URL + "/:path*",
+      },
+    ];
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.API_URL,
+  },
 };
 
 module.exports = nextConfig;
