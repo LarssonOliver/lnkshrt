2bccfabd2d4a9590c84f8a9149b9f2d97f459501
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-02-23 16:48:32 +0100

    gui wip

--- gui/next.config.js
+++ gui/next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: process.env.API_URL + "/:path*",
-      },
-    ];
-  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.API_URL,
+  },
+  experimental: {
+    outputStandalone: true,
   },
 };
 
---
+++ gui/pages/_middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  if (!url.pathname.startsWith("/api")) return NextResponse.next();
+
+  // Rewrite all /api urls.
+  const path = url.pathname.replace("/api", "");
+  const rewrite = `${process.env.API_URL}${path}`;
+
+  return NextResponse.rewrite(rewrite);
+}
