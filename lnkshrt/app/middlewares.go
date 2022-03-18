a5d7869a11815ea2a58b30c532098d59c5b353a2
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-03-18 12:23:04 +0100

    middleware now logs request processing time

--- lnkshrt/app/middlewares.go
+++ lnkshrt/app/middlewares.go
@@ -3,11 +3,28 @@ package app
 import (
 	"log"
 	"net/http"
+	"runtime/debug"
+	"time"
 )
 
 func LoggingMiddleware(n http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method, r.URL.Path)
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				log.Printf("panic: %v\nstacktrace: %v\n", err, debug.Stack())
+			}
+		}()
+
+		start := time.Now()
 		n.ServeHTTP(w, r)
-	})
+		log.Println(
+			// w.Header().Get("Status"),
+			r.Method,
+			r.URL.EscapedPath(),
+			time.Since(start),
+		)
+	}
+
+	return http.HandlerFunc(fn)
 }
