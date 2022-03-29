deadc797411f6151ff0d840c353bd1b6d6e660ad
Oliver <c18ols@cs.umu.se>
2022-03-29 16:06:34 +0200

    moved logic from cmd to internal

--- cmd/lnkshrt/main.go
+++ cmd/lnkshrt/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	"github.com/larssonoliver/lnkshrt/internal/app"
 	"github.com/larssonoliver/lnkshrt/internal/config"
 )
@@ -15,19 +11,7 @@ func logInit() {
 	log.SetPrefix("\033[35m[" + config.Executable() + "]:\033[0m ")
 }
 
-func corsSetup(router *mux.Router) http.Handler {
-	methods := handlers.AllowedMethods([]string{"GET", "POST"})
-	ttl := handlers.MaxAge(3600)
-	origins := handlers.AllowedOrigins(config.Origins())
-	return handlers.CORS(origins, methods, ttl)(router)
-}
-
 func main() {
 	logInit()
-	app := app.New()
-
-	port := fmt.Sprintf(":%d", config.Port())
-
-	log.Println("Listening on port", port)
-	log.Fatal(http.ListenAndServe(port, corsSetup(app.Router)))
+	app.New().Run()
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/larssonoliver/lnkshrt/internal/config"
 	"github.com/larssonoliver/lnkshrt/internal/db"
@@ -26,4 +29,18 @@ func New() *App {
 
 	a.initRoutes()
 	return a
+}
+
+func (a *App) Run() {
+	port := fmt.Sprintf(":%d", config.Port())
+
+	log.Println("Listening on port", port)
+	log.Fatal(http.ListenAndServe(port, corsSetup(a.Router)))
+}
+
+func corsSetup(router *mux.Router) http.Handler {
+	methods := handlers.AllowedMethods([]string{"GET", "POST"})
+	ttl := handlers.MaxAge(3600)
+	origins := handlers.AllowedOrigins(config.Origins())
+	return handlers.CORS(origins, methods, ttl)(router)
 }
